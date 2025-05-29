477164e2f749307519e26808f6e99a5164e0512e
mateusrtw1 <mateusristow16@gmail.com>
2025-05-29 17:02:27 -0300

    Add files via upload

---
+++ adivinhação.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+
+    int numUsuario;
+    int contador = 0;
+
+    while(numUsuario != 82){
+            printf("\nDigite um numero de 0 a 100.\n");
+            scanf("%i", &numUsuario);
+
+        if(numUsuario > 10){
+            printf("O numero eh menor que esse.\n");
+        } else if(numUsuario < 10){
+            printf("O numero eh maior que esse.\n");
+        }
+        contador++;
+    }
+    printf("\nParabens, voce acertou, o numero era 10!\n");
+    printf("Voce tentou %i vez(es).\n", contador);
+}
